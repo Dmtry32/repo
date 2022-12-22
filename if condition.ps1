c748c71c781fb7adf7b4e2c86a514be5c41dd2b3
anas moussaoui <mogat132@gmail.com>
2022-12-22 22:00:14 +0100

    intial

---
+++ if condition.ps1
@@ -0,0 +1,16 @@
+﻿cls
+[string ] $nomclasse = "cp"
+[int] $nombreeleve = 28
+
+#test de foctionement de variable 
+
+
+Write-Host "la class $nomclasse a $nombreeleve eleve dans sa class "
+if ($nombreeleve -le 15 ) { Write-output "il n ya pas beaucup deleve "}
+
+elseif ( $nombreeleve -ge 25 ) { Write-Output " il y a moin eleve dans cette class " }
+
+else { Write-Host "vous etes toujours aussi beau"}
+
+
+
